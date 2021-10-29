372f770dd6a054ab06aed83a667938339a6e1110
s.fedorovich <s.fedorovich@CZC71776GT>
2021-10-29 11:43:22 +0300

    Задача 2

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Задача_2
 {
@@ -10,6 +11,24 @@ namespace Задача_2
     {
         static void Main(string[] args)
         {
+            string path = @"C:\users\s.fedorovich\desktop\c#\занятие 8\Proba_pera.txt";
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                int[] array = new int[10];
+                Random random = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(random.Next(0, 10));
+                }
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                int[] SummFile = File.ReadAllLines(path).Select(int.Parse).ToArray();
+                int sum = SummFile.Sum();
+                Console.WriteLine(sr.ReadToEnd());
+                Console.WriteLine("Сумма чисел в файле: {0}", sum);
+            }
+            Console.ReadKey();
         }
     }
 }
